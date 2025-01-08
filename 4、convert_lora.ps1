cd51f9999e11a4a28bcca0289661845bc94c249a
sdbds <865105819@qq.com>
2025-01-08 20:33:14 +0800

    update convert script

---
+++ 4、convert_lora.ps1
@@ -0,0 +1,38 @@
+$input_path="./output_dir/hyvideo-qinglong.safetensors"
+$output_path="./output_dir/hyvideo-qinglong_comfy.safetensors"
+$target="other" # "other" or "default"
+
+# ============= DO NOT MODIFY CONTENTS BELOW | 请勿修改下方内容 =====================
+# Activate python venv
+Set-Location $PSScriptRoot
+if ($env:OS -ilike "*windows*") {
+    if (Test-Path "./venv/Scripts/activate") {
+        Write-Output "Windows venv"
+        ./venv/Scripts/activate
+    }
+    elseif (Test-Path "./.venv/Scripts/activate") {
+        Write-Output "Windows .venv"
+        ./.venv/Scripts/activate
+    }
+}
+elseif (Test-Path "./venv/bin/activate") {
+    Write-Output "Linux venv"
+    ./venv/bin/Activate.ps1
+}
+elseif (Test-Path "./.venv/bin/activate") {
+    Write-Output "Linux .venv"
+    ./.venv/bin/activate.ps1
+}
+
+$Env:HF_HOME = "huggingface"
+#$Env:HF_ENDPOINT = "https://hf-mirror.com"
+$Env:XFORMERS_FORCE_DISABLE_TRITON = "1"
+$ext_args = [System.Collections.ArrayList]::new()
+
+# run Cache
+python "./musubi-tuner/convert_lora.py" --input=$input_path `
+    --output=$output_path `
+    --target=$target $ext_args
+
+Write-Output "Cache finished"
+Read-Host | Out-Null ;
